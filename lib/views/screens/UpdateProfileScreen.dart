b6111b32238519a873a2f46f29e5311abfb582e9
RnaYuta <rnayuta37@gmail.com>
2024-08-03 00:32:12 +0900

    내 정보 수정 "수정 완료" 다이얼 로그 추가

--- lib/views/screens/UpdateProfileScreen.dart
+++ lib/views/screens/UpdateProfileScreen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:usw_circle_link/views/screens/MainScreen.dart';
+import 'package:usw_circle_link/views/widgets/RoundedDropdown.dart';
 import 'package:usw_circle_link/views/widgets/RoundedTextField.dart';
+import 'package:usw_circle_link/views/widgets/TextFontWidget.dart';
 
 class Updateprofilescreen extends StatefulWidget {
   const Updateprofilescreen({super.key});
@@ -15,6 +18,11 @@ class _UpdateprofilescreenState extends State<Updateprofilescreen> {
   TextEditingController phonenumberController = TextEditingController();
   TextEditingController studentnumberController = TextEditingController();
   TextEditingController departmentController = TextEditingController();
+  String? college;
+  String? major;
+
+  String? selectedCollege;
+  String? selectedMajor;
 
   @override
   Widget build(BuildContext context) {
@@ -104,6 +112,15 @@ class _UpdateprofilescreenState extends State<Updateprofilescreen> {
                       maxLines: 1,
                       textInputType: TextInputType.text,
                       textAlign: TextAlign.left,
+                      hintText: "이름",
+                      prefixIcon: SvgPicture.asset(
+                        'assets/images/ic_person.svg',
+                        width: 13.w,
+                        height: 16.h,
+                        fit: BoxFit.scaleDown,
+                      ),
+                      hintStyle: TextStyle(
+                          fontSize: 14.sp, fontFamily: 'Pretendard-Regular'),
                     ),
                   )
                 ],
@@ -134,7 +151,7 @@ class _UpdateprofilescreenState extends State<Updateprofilescreen> {
                     child:
                     RoundedTextField(
                       height: 50.h,
-                      textEditController: nameController,
+                      textEditController: phonenumberController,
                       leftBottomCornerRadius: 8.r,
                       rightBottomCornerRadius: 8.r,
                       leftTopCornerRadius: 8.r,
@@ -144,6 +161,15 @@ class _UpdateprofilescreenState extends State<Updateprofilescreen> {
                       maxLines: 1,
                       textInputType: TextInputType.text,
                       textAlign: TextAlign.left,
+                      prefixIcon: SvgPicture.asset(
+                        'assets/images/ic_phone.svg',
+                        width: 13.w,
+                        height: 16.h,
+                        fit: BoxFit.scaleDown,
+                      ),
+                      hintText: "전화번호 (- 제외입력)",
+                      hintStyle: TextStyle(
+                          fontSize: 14.sp, fontFamily: 'Pretendard-Regular'),
                     ),
                   )
                 ],
@@ -174,7 +200,7 @@ class _UpdateprofilescreenState extends State<Updateprofilescreen> {
                     child:
                     RoundedTextField(
                       height: 50.h,
-                      textEditController: nameController,
+                      textEditController: studentnumberController,
                       leftBottomCornerRadius: 8.r,
                       rightBottomCornerRadius: 8.r,
                       leftTopCornerRadius: 8.r,
@@ -184,6 +210,15 @@ class _UpdateprofilescreenState extends State<Updateprofilescreen> {
                       maxLines: 1,
                       textInputType: TextInputType.text,
                       textAlign: TextAlign.left,
+                      prefixIcon: SvgPicture.asset(
+                        'assets/images/ic_tag.svg',
+                        width: 13.w,
+                        height: 16.h,
+                        fit: BoxFit.scaleDown,
+                      ),
+                      hintText: "학번",
+                      hintStyle: TextStyle(
+                          fontSize: 14.sp, fontFamily: 'Pretendard-Regular'),
                     ),
                   )
                 ],
@@ -214,7 +249,10 @@ class _UpdateprofilescreenState extends State<Updateprofilescreen> {
                     child:
                     RoundedTextField(
                       height: 50.h,
-                      textEditController: nameController,
+                      readOnly: true,
+                      onTab: () async {
+                        showCustomDialog(context);
+                      },
                       leftBottomCornerRadius: 8.r,
                       rightBottomCornerRadius: 8.r,
                       leftTopCornerRadius: 8.r,
@@ -222,19 +260,198 @@ class _UpdateprofilescreenState extends State<Updateprofilescreen> {
                       borderColor: const Color(0xffDBDBDB),
                       borderWidth: 1.w,
                       maxLines: 1,
-                      textInputType: TextInputType.text,
+                      textInputType: TextInputType.none,
                       textAlign: TextAlign.left,
+                      textInputAction: TextInputAction.done,
+                      prefixIcon: SvgPicture.asset(
+                        'assets/images/ic_bookmark.svg',
+                        width: 13.w,
+                        height: 16.h,
+                        fit: BoxFit.scaleDown,
+                      ),
+                      suffixIcon: SvgPicture.asset(
+                        'assets/images/>.svg',
+                        width: 32.w,
+                        height: 32.h,
+                        fit: BoxFit.scaleDown,
+                      ),
+                      hintText: (selectedCollege == null || selectedMajor == null)?'학과':'${selectedMajor}',
+                      hintStyle: TextStyle(
+                          fontSize: 14.sp, fontFamily: 'Pretendard-Regular'),
+                      isAnimatedHint: false,
                     ),
                   )
                 ],
               ),
               SizedBox(height: 48.h,),
-              CustomButton(text: '수정 완료', onPressed: () {}),
+              CustomButton(text: '수정 완료', onPressed: () {
+                showalarmCustomDialog(context);
+              }),
             ],
           ),
         ),
       ),
     );
+  }
+  void showCustomDialog(BuildContext context) async {
+    final result = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.r),
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                RoundedDropdown(
+                  initValue: selectedCollege,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedCollege = newValue;
+                    });
+                  },
+                  items: <String>['College 1', 'College 2', 'College 3'],
+                  hintText: '단과대학 선택',
+                  leftTopCornerRadius: 8.r,
+                  leftBottomCornerRadius: 8.r,
+                  rightTopCornerRadius: 8.r,
+                  rightBottomCornerRadius: 8.r,
+                  borderColor: Color(0xFFCECECE),
+                  borderWidth: 1.w,
+                  marginTop: 32.h,
+                  marginLeft: 16.w,
+                  marginRight: 16.w,
+                ),
+                SizedBox(height: 8),
+                RoundedDropdown(
+                  initValue: selectedMajor,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedMajor = newValue;
+                    });
+                  },
+                  items: <String>['Major 1', 'Major 2', 'Major 3'],
+                  hintText: '학부(학과) 선택',
+                  leftTopCornerRadius: 8.r,
+                  leftBottomCornerRadius: 8.r,
+                  rightTopCornerRadius: 8.r,
+                  rightBottomCornerRadius: 8.r,
+                  borderColor: Color(0xFFCECECE),
+
+                  borderWidth: 1.w,
+                  marginLeft: 16.w,
+                  marginRight: 16.w,
+                ),
+                SizedBox(height: 20),
+                SizedBox.fromSize(
+                  size: Size.fromHeight(1.h),
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: Color(0xFFCECECE)),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(
+                        {'college': selectedCollege, 'major': selectedMajor});
+                  },
+                  style: TextButton.styleFrom(
+                    minimumSize: Size.fromHeight(50.h),
+                    //primary: Colors.white,
+                    //onPrimary: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                          top: Radius.zero, bottom: Radius.circular(15.r)),
+                    ),
+                  ),
+                  child: TextFontWidget.fontRegular(
+                    text: "확인",
+                    color: Color(0xFF0085FF),
+                    fontSize: 18.sp,
+                    fontweight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+
+    setState(() {
+      if (result != null) {
+        college = result['college'];
+        major = result['major'];
+        debugPrint('${college} - ${major}');
+        debugPrint('${selectedCollege} - ${selectedMajor}');
+      }
+    });
+  }
+
+  void showalarmCustomDialog(BuildContext context) async {
+    final result = showDialog(context: context, builder: (BuildContext context) {
+      return Dialog(
+        child: Container(
+          padding: EdgeInsets.zero,
+          width: 270.w,
+          height: 125.h,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.r),
+            color: const Color(0xffffffff),
+
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 15.h,),
+              Text("알림" , style: TextStyle(
+                color: const Color(0xff111111),
+                fontWeight: FontWeight.w600,
+                fontSize: 18.sp,
+                fontFamily: "Pretendard",
+                fontStyle: FontStyle.normal,
+                height: 1.11.sp,
+              ),),
+              SizedBox(height: 8.h,),
+              Text("내 정보가 수정되었습니다", style: TextStyle(
+                color: const Color(0xff767676),
+                fontWeight: FontWeight.w400,
+                fontSize: 14.sp,
+                fontFamily: "Pretendard",
+                fontStyle: FontStyle.normal,
+                height: 1.14.sp,
+              ),),
+              SizedBox(height: 15.h,),
+              Container(
+                  width: 275.w,
+                  height: 1.h,
+                  decoration:     BoxDecoration(
+                      color: Color(0xffdadada))
+              ),
+              SizedBox(height: 1.h,),
+              TextButton(onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainScreen()),);
+              }, child: Text("확인", style: TextStyle(
+                color: const Color(0xff0085FF),
+                fontWeight: FontWeight.w500,
+                fontSize: 18.sp,
+                fontFamily: "Pretendard",
+                fontStyle: FontStyle.normal,
+                height: 1.11.sp,
+              ),),
+                style: ButtonStyle(
+
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
 class CustomButton extends StatelessWidget {
@@ -270,4 +487,5 @@ class CustomButton extends StatelessWidget {
       ),
     );
   }
+
 }
