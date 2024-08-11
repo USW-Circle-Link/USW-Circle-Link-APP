d037cc6ec65cace153768154a5372e6c29371471
이수빈 <97301965+LeeSubin3020@users.noreply.github.com>
2024-08-11 22:54:59 +0900

    fcm 메세지 테스트 & riverpod
    fcm 메세지 테스트 & riverpod 상태 관리 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:usw_circle_link/views/screens/MainScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // Riverpod 패키지 추가
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const CircleLink());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  String? fcmToken = await FirebaseMessaging.instance.getToken();
+  print("FCM Token: $fcmToken");
+
+  runApp(const ProviderScope(child: CircleLink())); // ProviderScope로 앱을 래핑
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
 }
 
 class CircleLink extends StatelessWidget {
   const CircleLink({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/views/screens/MainScreen.dart
+++ lib/views/screens/MainScreen.dart
@@ -1,21 +1,52 @@
-//import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:usw_circle_link/riverpod/notification_state_notifier.dart';
+import 'package:usw_circle_link/view_models/FirbaseVM.dart';
 import 'package:usw_circle_link/views/screens/LoginScreen.dart';
+import 'package:usw_circle_link/views/widgets/CloudMessaging.dart';
 import 'package:usw_circle_link/views/widgets/TextFontWidget.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends ConsumerStatefulWidget {
   MainScreen({super.key});
 
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends ConsumerState<MainScreen> {
   bool isAllSelected = true;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  OverlayEntry? _overlayEntry;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // ViewModel을 통해 FCM 초기화
+    ref.read(notificationViewModelProvider).initializeFCM();
+  }
+
+  void _showOverlay(BuildContext context) {
+    if (_overlayEntry == null) {
+      _overlayEntry = _createOverlayEntry(context);
+      Overlay.of(context)?.insert(_overlayEntry!);
+    } else {
+      (_overlayEntry!.builder as _NotificationOverlayState).updateList();
+    }
+  }
+
+  OverlayEntry _createOverlayEntry(BuildContext context) {
+    return OverlayEntry(
+      builder: (context) => _NotificationOverlay(
+        onDismiss: () {
+          _overlayEntry?.remove();
+          _overlayEntry = null;
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,13 +62,14 @@ class _MainScreenState extends State<MainScreen> {
           elevation: 0.0,
           backgroundColor: Colors.white,
           leading: Padding(
-            padding: EdgeInsets.only(left: 24.w), // menubt.svg에 왼쪽 여백 추가
+            padding: EdgeInsets.only(left: 24.w),
             child: IconButton(
               onPressed: () {
                 _scaffoldKey.currentState?.openDrawer();
               },
-              icon: SvgPicture.asset(
-                'assets/images/menubt.svg',
+              icon: Icon(
+                Icons.menu_outlined,
+                color: Colors.grey,
               ),
             ),
           ),
@@ -49,7 +81,7 @@ class _MainScreenState extends State<MainScreen> {
                 width: 19.w,
                 height: 19.h,
               ),
-              SizedBox(width: 8.w), // 이미지와 텍스트 사이의 간격 조절
+              SizedBox(width: 8.w),
               TextFontWidget.jalnan2(
                 text: '동구라미',
                 fontSize: 16.sp,
@@ -60,12 +92,12 @@ class _MainScreenState extends State<MainScreen> {
           ),
           actions: [
             Padding(
-              padding: EdgeInsets.only(right: 24.w), // bell.svg에 오른쪽 여백 추가
+              padding: EdgeInsets.only(right: 24.w),
               child: IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset(
-                  'assets/images/bell.svg',
-                ),
+                onPressed: () {
+                  _showOverlay(context);
+                },
+                icon: SvgPicture.asset('assets/images/bell.svg'),
               ),
             ),
           ],
@@ -117,10 +149,10 @@ class _MainScreenState extends State<MainScreen> {
                     ),
                   ),
                   child: TextFontWidget.jalnan2(
-                    text: '모집 중',
+                    text: '모집중',
                     fontSize: 16.sp,
                     color:
-                        isAllSelected ? const Color(0xffCECECE) : Colors.white,
+                        !isAllSelected ? Colors.white : const Color(0xffCECECE),
                     fontweight: FontWeight.w400,
                   ),
                 ),
@@ -144,76 +176,222 @@ class _MainScreenState extends State<MainScreen> {
   }
 }
 
+class _NotificationOverlay extends StatefulWidget {
+  final VoidCallback onDismiss;
+
+  _NotificationOverlay({required this.onDismiss});
+
+  @override
+  _NotificationOverlayState createState() => _NotificationOverlayState();
+}
+
+class _NotificationOverlayState extends State<_NotificationOverlay> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: GestureDetector(
+            onTap: widget.onDismiss,
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
+        ),
+        Positioned(
+          top: kToolbarHeight + 45.h,
+          right: 24.w,
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              width: 208.w,
+              height: 220.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.r),
+                color: Colors.white,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16.h),
+                    child: Text(
+                      "알림",
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Consumer(builder: (context, ref, child) {
+                      final notifications = ref.watch(notificationProvider);
+                      return ListView.builder(
+                        itemCount: notifications.length,
+                        itemBuilder: (context, index) {
+                          return CloudMessaging(
+                            text: notifications[index],
+                            index: index,
+                          );
+                        },
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void updateList() {
+    setState(() {});
+  }
+}
+
 // ignore: non_constant_identifier_names
+bool isMyInfoExpanded = false; // 내 정보가 확장되었는지 여부를 확인
+
 Widget Menubar(BuildContext context) {
   return Drawer(
     backgroundColor: const Color(0xffF0F2F5),
     width: 290.w,
-    child: ListView(
-      padding: EdgeInsets.zero,
+    child: Column(
       children: [
-        DrawerHeader(
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child: GestureDetector(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
-            },
-            child: Center(
-                child: Row(
-              children: [
-                SizedBox(width: 3.w),
-                SvgPicture.asset(
-                  'assets/images/profile.svg',
+        Expanded(
+          // ListView를 Expanded로 감싸서 가변적 높이를 할당
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.white,
                 ),
-                SizedBox(width: 16.w),
-                TextFontWidget.fontRegular(
-                    text: '로그인',
-                    fontSize: 18.sp,
-                    color: Colors.black,
-                    fontweight: FontWeight.w600),
-                SvgPicture.asset(
-                  'assets/images/>.svg',
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  },
+                  child: Center(
+                      child: Row(
+                    children: [
+                      SizedBox(width: 3.w),
+                      SvgPicture.asset(
+                        'assets/images/profile.svg',
+                      ),
+                      SizedBox(width: 16.w),
+                      TextFontWidget.fontRegular(
+                          text: '로그인',
+                          fontSize: 18.sp,
+                          color: Colors.black,
+                          fontweight: FontWeight.w600),
+                      SvgPicture.asset(
+                        'assets/images/>.svg',
+                      ),
+                    ],
+                  )),
+                ),
+              ),
+              // 내 정보 부분 수정 시작
+              SizedBox(height: 10.h),
+              buildDrawerItem(
+                title: '내 정보',
+                svgPath: 'assets/images/menubar1.svg',
+                onTap: () {
+                  // 내 정보 부분 클릭 시 확장 상태 변경
+                  isMyInfoExpanded = !isMyInfoExpanded;
+                  // 상태 갱신
+                  (context as Element).markNeedsBuild();
+                },
+                trailingSvgPath: isMyInfoExpanded
+                    ? 'assets/images/under<.svg'
+                    : 'assets/images/>.svg',
+                isExpanded:
+                    isMyInfoExpanded, // 내 정보가 확장되었을 때 마진과 border radius를 없애기 위해 전달
+              ),
+              // 내 정보 확장 시 나타나는 항목들
+              if (isMyInfoExpanded) ...[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.w),
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 12.h),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(8.r),
+                      ), // 확장된 경우 위쪽 radius는 제거, 아래쪽만 둥글게 유지
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: TextFontWidget.fontRegular(
+                            text: '내 정보 수정',
+                            fontSize: 12.sp,
+                            color: const Color(0xff353549),
+                            fontweight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16.h,
+                          child: VerticalDivider(
+                            color: Color(0xffCECECE),
+                            thickness: 1,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: TextFontWidget.fontRegular(
+                            text: '비밀번호 변경',
+                            fontSize: 12.sp,
+                            color: const Color(0xff353549),
+                            fontweight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
-            )),
+              // 내 정보 부분 수정 끝
+              buildDrawerItem(
+                title: '나의 소속 동아리',
+                svgPath: 'assets/images/menubar2.svg',
+                onTap: () {
+                  // 나의 소속 동아리 화면으로 이동
+                },
+                trailingSvgPath: 'assets/images/>.svg', // 추가된 부분
+              ),
+              buildDrawerItem(
+                title: '지원 현황 확인하기',
+                svgPath: 'assets/images/menubar3.svg',
+                onTap: () {
+                  // 지원 현황 확인 화면으로 이동
+                },
+                trailingSvgPath: 'assets/images/>.svg', // 추가된 부분
+              ),
+              buildDrawerItem(
+                title: '공지 사항',
+                svgPath: 'assets/images/menubar4.svg',
+                onTap: () {
+                  // 공지 사항 화면으로 이동
+                },
+                trailingSvgPath: 'assets/images/>.svg', // 추가된 부분
+              ),
+            ],
           ),
         ),
-        buildDrawerItem(
-          title: '내 정보',
-          svgPath: 'assets/images/menubar1.svg',
-          onTap: () {
-            // 내 정보 화면으로 이동
-          },
-        ),
-        buildDrawerItem(
-          title: '나의 소속 동아리',
-          svgPath: 'assets/images/menubar2.svg',
-          onTap: () {
-            // 나의 소속 동아리 화면으로 이동
-          },
-        ),
-        buildDrawerItem(
-          title: '지원 현황 확인하기',
-          svgPath: 'assets/images/menubar3.svg',
-          onTap: () {
-            // 지원 현황 확인 화면으로 이동
-          },
-        ),
-        buildDrawerItem(
-          title: '공지 사항',
-          svgPath: 'assets/images/menubar4.svg',
-          onTap: () {
-            // 공지 사항 화면으로 이동
-          },
-        ),
+        // 하단 버튼을 화면 하단에 고정
         Column(
           children: [
             Container(
-              height: 400.h,
+              height: 150.h,
               padding: EdgeInsets.symmetric(horizontal: 24.w),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   TextButton(
                     onPressed: () {},
@@ -233,7 +411,7 @@ Widget Menubar(BuildContext context) {
                   ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ],
@@ -245,29 +423,40 @@ Widget buildDrawerItem({
   required String title,
   required String svgPath,
   required VoidCallback onTap,
+  required String trailingSvgPath,
+  bool isExpanded = false, // 확장 상태 전달받기
 }) {
   return Container(
-    margin: EdgeInsets.symmetric(horizontal: 24.w, vertical: 12.h),
+    alignment: Alignment.center,
+    margin: EdgeInsets.only(
+      left: 24.w,
+      right: 24.w,
+      bottom: isExpanded ? 0.h : 12.h,
+    ), // 확장 시 마진 제거
     width: 242.w,
     height: 56.h,
     decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(8.r),
+      borderRadius: isExpanded
+          ? BorderRadius.vertical(
+              top: Radius.circular(8.r),
+              bottom: Radius.circular(0),
+            )
+          : BorderRadius.circular(8.r), // 확장 시 위쪽 radius 제거
       color: const Color(0xffFFFFFF),
     ),
     child: ListTile(
-      leading: Padding(
-        padding: EdgeInsets.only(left: 16.w),
-        child: SvgPicture.asset(svgPath),
-      ),
+      contentPadding: EdgeInsets.only(left: 16.w, right: 6), // 패딩 조정
+      leading: SvgPicture.asset(svgPath),
       title: Padding(
         padding: EdgeInsets.only(left: 10.w),
         child: TextFontWidget.fontRegular(
           text: title,
-          fontSize: 16.sp,
+          fontSize: 15.sp,
           color: const Color(0xff353549),
-          fontweight: FontWeight.w400,
+          fontweight: FontWeight.w500,
         ),
       ),
+      trailing: SvgPicture.asset(trailingSvgPath),
       onTap: onTap,
     ),
   );
