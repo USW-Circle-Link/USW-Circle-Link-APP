97148f37b829e48c6b57d1e81c7692fdafe7e8a1
Woochang4862 <Woochang4862@users.noreply.github.com>
2025-09-24 08:21:17 +0000

    deploy: 9db712c3a516b2a9faaeceeae54216d3d37cd82f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1818455270"
+    serviceWorkerVersion: "4160265013"
   }
 });
