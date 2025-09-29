c43ee21079c0db0f321dac1294f0d286104bd149
RnaYuta <RnaYuta@users.noreply.github.com>
2025-09-29 15:10:14 +0000

    deploy: 5c822caa422c2fc2a884129ea1b0a50f52cf5ae1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1644022158"
+    serviceWorkerVersion: "524181350"
   }
 });
