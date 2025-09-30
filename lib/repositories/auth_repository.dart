fbdf0f8e5dec97a98dcaf9a5e5953f4640528178
Doubtagain <jamechoi0325@icloud.com>
2025-09-30 15:31:41 +0900

    feat: Modify Request OPTION for backend data (get => post)

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -108,7 +108,7 @@ class AuthRepository {
   }
 
   Future<bool> verifyEmail({required String email}) async {
-    final response = await dio.post(
+    final response = await dio.get(
       '$basePath/check/email/duplicate',
       data: {
         'email': email,
@@ -351,7 +351,7 @@ class AuthRepository {
     final body = {
       "authCode": code,
     };
-    final response = await dio.post(
+    final response = await dio.get(
       '$basePath/auth/verify-token',
       data: body,
       options: Options(
