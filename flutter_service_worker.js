1a6c5b98e89db77136f1f6c5ae64206dd3f8ab88
Doubtagain <Doubtagain@users.noreply.github.com>
2025-09-30 11:52:26 +0000

    deploy: bdeb624c82a705ce49f398e0af47edf629836349

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,8 +9,8 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "d12d0f0172b4ff7f81820bc73d0dd6ff",
-"index.html": "157b64788e0269ab3e8278073a13b30c",
-"/": "157b64788e0269ab3e8278073a13b30c",
+"index.html": "b7a2332b68a20612eb712ad352f26778",
+"/": "b7a2332b68a20612eb712ad352f26778",
 "firebase-messaging-sw.js": "b86d8dc407c6ae1e8d54577a28a4d82b",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "475067c425b761f16a1d2b2221b96b19",
@@ -63,9 +63,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "5007d7a1abb980d6ec92cb31b4cd56e2",
+"flutter_bootstrap.js": "4267e804e1ce308c40ce91a445fbd0e9",
 "version.json": "3be0fc023c66c0452066a5b8686bb807",
-"main.dart.js": "ba247b6b2d913015e1caeca259f378c5"};
+"main.dart.js": "ed3de4c77472724855696724150853dc"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
