a6bc1d1275947def0770cff30304ec71eff4a200
정우창 <woochang4862@gmail.com>
2025-10-04 21:26:53 +0900

    fix : dio 에 withCredentials 설정 추가

--- lib/dio/Dio.dart
+++ lib/dio/Dio.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:usw_circle_link/const/data.dart';
 import 'package:usw_circle_link/secure_storage/secure_storage.dart';
@@ -8,13 +7,11 @@ import 'package:usw_circle_link/utils/interceptor/token_interceptor.dart';
 
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(
-    BaseOptions(
-      baseUrl: '$protocol://$host:$port',
-      headers: {
-        'Content-Type': 'application/json',
-        // if (kIsWeb) 'Access-Control-Allow-Origin': '*',
-      },
-    ),
+    BaseOptions(baseUrl: '$protocol://$host:$port', headers: {
+      'Content-Type': 'application/json',
+    }, extra: {
+      'withCredentials': true,
+    }),
   );
 
   final storage = ref.watch(secureStorageProvider);
