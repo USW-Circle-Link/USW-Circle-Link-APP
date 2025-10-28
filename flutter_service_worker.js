b49ccd584f417b9e549afe9b88dc10d7bf13df84
Woochang4862 <Woochang4862@users.noreply.github.com>
2025-10-28 09:07:34 +0000

    deploy: c2df5b20a4b00de6de4dd6fe22abc496c62b0384

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,8 +12,8 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "icons/android-chrome-192x192.png": "d4f5df96c57ff18aae89434bc677b167",
 "icons/Icon-maskable-192.png": "d4f5df96c57ff18aae89434bc677b167",
 "manifest.json": "029cb8b3c7745047fc31f9917698de3d",
-"index.html": "729d80c41a09dcf918b4ae3530b69505",
-"/": "729d80c41a09dcf918b4ae3530b69505",
+"index.html": "e559c8a3d1968511829c3742c40f3ab8",
+"/": "e559c8a3d1968511829c3742c40f3ab8",
 "firebase-messaging-sw.js": "b86d8dc407c6ae1e8d54577a28a4d82b",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "ad31b6611c93a6e02b56d514b45c62a2",
@@ -68,9 +68,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.ico": "7b9dfd781a82f8d626ab36a15f9f9284",
-"flutter_bootstrap.js": "3595005203ad7d62809655ad6286e642",
+"flutter_bootstrap.js": "a02cf9992f5281eaeb3ac65257ae0dca",
 "version.json": "3be0fc023c66c0452066a5b8686bb807",
-"main.dart.js": "0c80787430329ce1b3532e8d9afebdb1"};
+"main.dart.js": "fcfd4db3104113babdac231d1d224824"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
